4ad217039c4486dbf4f7b6cb82d3ccc9f8b513bc
Xin Xu <railwaycat@gmail.com>
2020-05-26 13:08:06 -0400

    not include melpa for package.el, no need.

--- conf/elpa.el
+++ conf/elpa.el
@@ -1,8 +1,7 @@
 ;; sources
 (require 'package)
 ;; GNU elpa includes by default
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("melpa" . "https://melpa.org/packages/")))
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")))
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/") t)
 (unless package--initialized (package-initialize))
