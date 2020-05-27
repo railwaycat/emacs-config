de01d35c67668bc5bc960a4d7294f93329c79e1d
Xin Xu <railwaycat@gmail.com>
2020-05-27 15:27:53 -0400

    remove ghost-style.el, adjust aka.el

--- conf/aka.el
+++ conf/aka.el
@@ -1,5 +1,8 @@
+;; load path for aka
+(add-to-list 'load-path (concat user-emacs-directory "aka-lisp"))
+
+;; perforce
 (el-get-bundle p4)
 
 ;; ghost setup
-(when (file-exists-p "~/.m_aka")
-  (load-file (concat user-emacs-directory "site-lisp/ghost-style.el")))
+(load-file (concat user-emacs-directory "aka-lisp/ghost-style.el"))
