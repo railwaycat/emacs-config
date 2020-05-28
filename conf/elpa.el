5a7960feb41e4abb6e52e32f66e920ba14443c8e
Xin Xu <railwaycat@gmail.com>
2020-05-28 10:34:59 -0400

    fix elpa

--- conf/elpa.el
+++ conf/elpa.el
@@ -1,7 +1,4 @@
-;; sources
-(require 'package)
-;; GNU elpa includes by default
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")))
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "https://melpa.org/packages/") t)
-(unless package--initialized (package-initialize))
+(package-initialize)
