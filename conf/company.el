a7ce41aabedbbcd267edcfd70eef4f69af7319a2
Xin Xu <railwaycat@gmail.com>
2020-06-02 23:06:33 -0400

    remove face attributes for company in conf file

--- conf/company.el
+++ conf/company.el
@@ -11,21 +11,6 @@
   ;; TAB to select
   (define-key company-active-map (kbd "C-i") 'company-complete-common-or-cycle)
 
-  (set-face-attribute 'company-tooltip nil
-                      :foreground "black" :background "lightgrey")
-  (set-face-attribute 'company-tooltip-common nil
-                      :foreground "black" :background "lightgrey")
-  (set-face-attribute 'company-tooltip-common-selection nil
-                      :foreground "white" :background "steelblue")
-  (set-face-attribute 'company-tooltip-selection nil
-                      :foreground "black" :background "steelblue")
-  (set-face-attribute 'company-preview-common nil
-                      :background nil :foreground "lightgrey" :underline t)
-  (set-face-attribute 'company-scrollbar-fg nil
-                      :background "orange")
-  (set-face-attribute 'company-scrollbar-bg nil
-                      :background "gray40")
-
   ;; global company mode
   (add-hook 'after-init-hook 'global-company-mode)
 
