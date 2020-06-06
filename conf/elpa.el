d9c944e891a709cf75bc489b189f68d8f756a05b
Xin Xu <railwaycat@gmail.com>
2020-06-06 13:14:28 -0400

    let el-get use use-package

--- conf/elpa.el
+++ conf/elpa.el
@@ -1,4 +1,5 @@
+;; use http for gnu repo as a workaround
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")))
-;; (add-to-list 'package-archives
-;;              '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
