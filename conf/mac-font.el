09993815e0518b851f42f5c3c9bd7a8e18a196f9
Xin Xu <railwaycat@gmail.com>
2020-06-17 13:08:48 -0400

    adjust functions to set font size

--- conf/mac-font.el
+++ conf/mac-font.el
@@ -4,59 +4,55 @@
 
 ;; CJK
 ;; 苹方
-(defun my/set-font-cjk-pingfang ()
+(defun my/set-font-cjk-pingfang (size)
   "Set CJK font to 苹方"
-  (interactive)
-    (dolist (charset '(han kana hangul cjk-misc symbol bopomofo))
-  (set-fontset-font t charset (font-spec :family "PingFang SC" :size 12))))
+  (dolist (charset '(han kana hangul cjk-misc symbol bopomofo))
+    (set-fontset-font t charset (font-spec :family "PingFang SC" :size size))))
 
 
 ;; 思源宋体
-(defun my/set-font-cjk-SourceHanSerif ()
+(defun my/set-font-cjk-SourceHanSerif (size)
   "Set CJK font to 思源宋体"
-  (interactive)
   (dolist (charset '(han cjk-misc symbol bopomofo))
-    (set-fontset-font t charset (font-spec :family "Source Han Serif SC" :size 14)))
+    (set-fontset-font t charset (font-spec :family "Source Han Serif SC" :size size)))
   (set-fontset-font t 'kana
-                    (font-spec :family "Source Han Serif" :size 14))
+                    (font-spec :family "Source Han Serif" :size size))
   (set-fontset-font t 'hangul
-                    (font-spec :family "Source Han Serif K" :size 14)))
+                    (font-spec :family "Source Han Serif K" :size size)))
 
 ;; 思源宋体
-(defun my/set-font-cjk-SourceHanSans ()
+(defun my/set-font-cjk-SourceHanSans (size)
   "Set CJK font to 思源黑体"
-  (interactive)
   (dolist (charset '(han cjk-misc symbol bopomofo))
-    (set-fontset-font t charset (font-spec :family "Source Han Sans SC" :size 12)))
+    (set-fontset-font t charset (font-spec :family "Source Han Sans SC" :size size)))
   (set-fontset-font t 'kana
-                    (font-spec :family "Source Han Sans" :size 12))
+                    (font-spec :family "Source Han Sans" :size size))
   (set-fontset-font t 'hangul
-                    (font-spec :family "Source Han Sans K" :size 12)))
+                    (font-spec :family "Source Han Sans K" :size size)))
 
 ;; 新书宋
-(defun my/set-font-cjk-xinshusong ()
+(defun my/set-font-cjk-xinshusong (size)
   "Set CJK font to 新书宋"
-  (interactive)
   (dolist (charset '(han cjk-misc symbol bopomofo))
-    (set-fontset-font t charset (font-spec :family "FZNewShuSong-Z10" :size 14)))
+    (set-fontset-font t charset (font-spec :family "FZNewShuSong-Z10" :size size)))
   (set-fontset-font t 'kana
-                    (font-spec :family "Source Han Serif" :size 14))
+                    (font-spec :family "Source Han Serif" :size size))
   (set-fontset-font t 'hangul
-                    (font-spec :family "Source Han Serif K" :size 14)))
+                    (font-spec :family "Source Han Serif K" :size size)))
 
 (defun my/set-font-normal ()
   "Using normal size font"
   (interactive)
   (set-face-attribute 'default nil :font (font-spec :name "PragmataPro Mono Liga" :size 12))
   (set-face-attribute 'fixed-pitch nil :font (font-spec :name "PragmataPro Mono Liga" :size 12))
-  (my/set-font-cjk-pingfang))
+  (my/set-font-cjk-pingfang 12))
 
 (defun my/set-font-large ()
   "Using large size font"
   (interactive)
   (set-face-attribute 'default nil :font (font-spec :name "JetBrains Mono" :size 12))
   (set-face-attribute 'fixed-pitch nil :font (font-spec :name "JetBrains Mono" :size 12))
-  (my/set-font-cjk-xinshusong))
+  (my/set-font-cjk-xinshusong 14))
 
 ;; Emoji
 (set-fontset-font t nil (font-spec :family "Apple Color Emoji") nil 'append)
