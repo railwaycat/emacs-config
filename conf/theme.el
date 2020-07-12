f331990823dd4413380f7b0ebd097440e5662010
Xin Xu <railwaycat@gmail.com>
2020-07-12 16:03:53 -0400

    not specify any theme by default

--- conf/theme.el
+++ conf/theme.el
@@ -2,7 +2,3 @@
 ;; customized solarized
 (add-to-list 'load-path (concat user-emacs-directory "themes/solarized"))
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/solarized"))
-
-;; load basic theme as default
-(load-theme 'basic t)
-
