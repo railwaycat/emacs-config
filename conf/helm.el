6d0c747242ddd262f95dac9f575ff8023d4f5f0f
Xin Xu <railwaycat@gmail.com>
2020-08-31 15:42:28 -0400

    use the latest helm to pickup fixes for modeline icon

--- conf/helm.el
+++ conf/helm.el
@@ -1,4 +1,4 @@
-(el-get-bundle helm :checkout "v3.6.4")
+(el-get-bundle helm)
 (use-package helm
   :init
   (require 'helm-config)
@@ -11,8 +11,7 @@
   ("C-c C-r" . helm-resume)
   ([remap switch-to-buffer] . helm-mini)
   :config
-  (setq helm-mode-fuzzy-match t
-        helm-ff-cache-mode-lighter nil)
+  (setq helm-mode-fuzzy-match t)
   (helm-mode 1))
 
 (el-get-bundle helm-gtags)
