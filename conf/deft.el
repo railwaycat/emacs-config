c5b48459902d9860c67adeacaf5298b084e0c976
Xin Xu <railwaycat@gmail.com>
2020-08-31 23:29:21 -0400

    deft only use Dropbox when available

--- conf/deft.el
+++ conf/deft.el
@@ -3,8 +3,8 @@
   :bind
   ("C-c d" . deft)
   :config
-  (setq deft-directory "~/Dropbox/notes/deft"
-        ;; deft-recursive t
-        deft-extensions '("org" "md" "markdown" "txt")
+  (setq deft-extensions '("org" "md" "markdown" "txt")
         deft-markdown-mode-title-level 1
-        deft-default-extension "md"))
+        deft-default-extension "md")
+  (when user-with-dropbox
+    (setq deft-directory "~/Dropbox/notes/deft")))
