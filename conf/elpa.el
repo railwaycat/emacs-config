3629c0d7dc993664baf132f061a6f5559ffacddc
Xin Xu <railwaycat@gmail.com>
2020-09-20 20:09:15 -0400

    not use priorities for elpa. gnu switch to https

--- conf/elpa.el
+++ conf/elpa.el
@@ -1,13 +1,14 @@
 ;; use http for gnu repo as a workaround
 (setq package-enable-at-startup nil)
 (setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
+      '(("gnu" . "https://elpa.gnu.org/packages/")
         ;; ("melpa stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "https://melpa.org/packages/"))
-      package-archive-priorities
-      '(("gnu" . 10)
-        ;; ("melpa stable" . 7)
-        ("melpa" . 5)))
+      ;; package-archive-priorities
+      ;; '(("gnu" . 10)
+      ;;   ;; ("melpa stable" . 7)
+      ;;   ("melpa" . 5)))
+      )
 (package-initialize)
 
 ;; use-package
