8e7b7187cc4b73f6ea6318de7b0aeb54ed62dfbd
Xin Xu <railwaycat@gmail.com>
2020-10-31 18:42:34 -0400

    hippie expand config use use-package

--- conf/hippie.el
+++ conf/hippie.el
@@ -1,16 +1,18 @@
-(define-key global-map (kbd "M-/") 'hippie-expand)
-(defun sanityinc/dabbrev-friend-buffer (other-buffer)
-  (< (buffer-size other-buffer) (* 1 1024 1024)))
-(customize-set-variable 'dabbrev-friend-buffer-function
-                        'sanityinc/dabbrev-friend-buffer)
-(setq hippie-expand-try-functions-list
-      '(try-expand-all-abbrevs
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-dabbrev
-        try-expand-dabbrev-from-kill
-        try-expand-dabbrev-all-buffers
-        try-expand-list
-        try-expand-line
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
+(use-package hippie-expand
+  :bind
+  ([remap dabbrev-expand] . hippie-expand)
+  :config
+  (defun sanityinc/dabbrev-friend-buffer (other-buffer)
+    (< (buffer-size other-buffer) (* 1 1024 1024)))  
+  :custom
+  (hippie-expand-try-functions-list '(try-expand-all-abbrevs
+                                      try-expand-dabbrev
+                                      try-expand-dabbrev-all-buffers
+                                      try-expand-dabbrev-from-kill
+                                      try-complete-file-name-partially
+                                      try-complete-file-name
+                                      try-expand-list
+                                      try-expand-line
+                                      try-complete-lisp-symbol-partially
+                                      try-complete-lisp-symbol))
+  (dabbrev-friend-buffer-function 'sanityinc/dabbrev-friend-buffer))
