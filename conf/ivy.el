9f59991e92a462b0437939ae082af15a4351cf94
Xin Xu <railwaycat@gmail.com>
2020-10-31 22:53:12 -0400

    refactoring ivy config

--- conf/ivy.el
+++ conf/ivy.el
@@ -2,53 +2,66 @@
 
 (use-package ivy
   :ensure t
-  :init
-  (setq enable-recursive-minibuffers t
-        ivy-use-virtual-buffers t
-        ivy-count-format "%d/%d ")
   :hook (after-init . ivy-mode)
-  )
+  :bind
+  ("C-c C-r" . ivy-resume)
+  :custom
+  (ivy-count-format "%d/%d ")          ;; better counts
+  (ivy-use-virtual-buffers t)          ;; show recent files
+  (ivy-height 20)
+  (ivy-sort-max-size 3000)             ;; the default value 30000 is too large
+  ;; (ivy-fixed-height-minibuffer t)      ;; fixed height
+  (ivy-on-del-error-function 'ignore) ;; dont quit minibuffer when del-error
+  (enable-recursive-minibuffers t))
+
+(use-package swiper
+  :ensure t
+  :bind
+  ([remap isearch-forward] . swiper-isearch)
+  ("C-c o" . swiper)
+  (:map ivy-minibuffer-map
+        ("C-s" . swiper)))
 
 (use-package counsel
-  :after ivy
   :ensure t
-  :custom
-  (ivy-height 20)
-  ;; :config
-  ;; (setq ivy-height-alist
-  ;;       '((t
-  ;;          lambda (_caller)
-  ;;          (/ (frame-height) 2))))
-  :config
-  (setq counsel-base-command
-        "rg -i -M 120 --no-heading --line-number --color never %s %s"
-        counsel-find-file-at-point t)
+  :hook (ivy-mode . counsel-mode)
   :bind
-  ([remap execute-extended-command] . counsel-M-x)
   ([remap yank-pop] . counsel-yank-pop)
-  ([remap find-file] . counsel-find-file)
-  ([remap bookmark-jump] . counsel-bookmark)
-  ([remap switch-to-buffer] . ivy-switch-buffer)
-  ;; ([remap isearch-forward] . swiper-isearch)
-  ("C-c C-r" . ivy-resume)
-  ("C-c o" . swiper)
-  ;; ("C-c g" . counsel-git)
-  ;; ("C-c j" . counsel-git-grep)
-  ("C-c k" . counsel-rg)
+  ([remap swiper] . counsel-grep-or-swiper)
+  ("C-c g" . counsel-rg)
   ("C-x l" . counsel-locate)
-  ("C-c f" . counsel-fzf)
   ;; ([remap org-capture] . counsel-org-capture)
   ("<f2> i" . counsel-info-lookup-symbol)
   ("<f2> u" . counsel-unicode-char)
   (:map minibuffer-local-map
         ("C-r" . counsel-minibuffer-history))
-  (:map ivy-minibuffer-map
-        ("C-s" . swiper))
-  )
+  :config
+  (ivy-set-actions
+   'counsel-find-file
+   '(("j" find-file-other-window "other window")
+     ("f" find-file-other-frame "other frame")
+     ("b" counsel-find-file-cd-bookmark-action "cd bookmark")
+     ("x" counsel-find-file-extern "open externally")
+     ("r" counsel-find-file-as-root "open as root")
+     ("R" find-file-read-only "read only")
+     ("l" find-file-literally "open literally")
+     ("B" hexl-find-file "open file in binary mode")
+     ("k" counsel-find-file-delete "delete")
+     ("c" counsel-find-file-copy "copy file")
+     ("m" counsel-find-file-move "move or rename")
+     ("d" counsel-find-file-mkdir-action "mkdir")))
+  (setq counsel-grep-base-command
+        "rg -i -M 120 --no-heading --line-number --color never %s %s"
+        counsel-find-file-at-point t)
+  :custom
+  (counsel-preselect-current-file t)
+  (counsel-yank-pop-preselect-last t)
+  (counsel-yank-pop-separator "\n───────────\n")
+  (counsel-find-file-at-point t)
+  (counsel-find-file-ignore-regexp "\\(?:\\`\\(?:\\.\\|__\\)\\|elc\\|pyc$\\)"))
 
 (use-package ivy-xref
   :ensure t
-  :after ivy
   :init
   (when (>= emacs-major-version 27)
     (setq xref-show-definitions-function #'ivy-xref-show-defs))
@@ -56,13 +69,13 @@
 
 (use-package flyspell-correct-ivy
   :ensure t
-  :after (ivy flyspell-correct))
+  :after flyspell-correct)
 
 (use-package projectile
   :ensure t)
 (use-package counsel-projectile
   :ensure t
-  :after (ivy projectile)
+  :after (projectile counsel)
   :init
   (counsel-projectile-mode 1)
   :bind
