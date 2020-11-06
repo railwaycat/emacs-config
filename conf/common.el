f1d61bda43d0162f9d4ca4b7b7a7367846d99c2d
Xin Xu <railwaycat@gmail.com>
2020-11-06 10:30:18 -0500

    delight abbrev

--- conf/common.el
+++ conf/common.el
@@ -192,6 +192,10 @@
 (use-package eldoc
   :delight)
 
+;; abbrev
+(use-package abbrev
+  :delight)
+
 (define-key global-map (kbd "<f5>") 'goto-line)
 (define-key global-map (kbd "<f6>") 'display-line-numbers-mode)
 (define-key global-map (kbd "<f8>") 'rename-buffer)
