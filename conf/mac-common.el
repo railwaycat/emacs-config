92c50def8b34857480d04b8aadcc6883f196d477
Xin Xu <railwaycat@gmail.com>
2020-12-24 14:46:21 -0500

    remove workaround for mac mouse scroll

--- conf/mac-common.el
+++ conf/mac-common.el
@@ -12,12 +12,3 @@
       (setq visible-bell nil)
       )
   )
-
-(when (eq window-system 'mac)
-    (progn
-      (customize-set-value 'mac-mouse-wheel-smooth-scroll nil)
-      (setq scroll-conservatively 10000
-            mouse-wheel-scroll-amount '(2 ((shift) . 1)) ;; one line at a time
-            mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
-            mouse-wheel-follow-mouse 't ;; scroll window under mouse
-            )))
