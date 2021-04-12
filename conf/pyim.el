7b931b67b1785ff8eb05a30a9a5f1adc0c9a2466
Xin Xu <railwaycat@gmail.com>
2021-04-11 20:00:11 -0400

    add pyim, for cangjie5

---
+++ conf/pyim.el
@@ -0,0 +1,13 @@
+;; setup for pyim
+;; enable cangjie by default
+;; the buildin tsangchi is too hard to use
+(use-package pyim
+  :ensure t
+  :demand t
+  :config
+  (use-package pyim-cangjie5dict
+    :ensure t
+    :config
+    (pyim-cangjie5-enable))
+  (setq default-input-method "pyim")
+  (setq pyim-default-scheme 'cangjie))
