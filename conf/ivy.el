37c5278d558653b289d7c0aa511ae2c33a97168d
Xin Xu <railwaycat@gmail.com>
2021-04-13 13:17:50 -0400

    setup for ivy-rich

--- conf/ivy.el
+++ conf/ivy.el
@@ -9,7 +9,7 @@
   :custom
   (ivy-count-format "%d/%d ")          ;; better counts
   (ivy-use-virtual-buffers t)          ;; show recent files
-  (ivy-virtual-abbreviate 'abbreviate)
+  (ivy-virtual-abbreviate 'full)
   (ivy-height 20)
   (ivy-sort-max-size 3000)             ;; the default value 30000 is too large
   ;; (ivy-fixed-height-minibuffer t)      ;; fixed height
@@ -57,7 +57,8 @@
      ("d" counsel-find-file-mkdir-action "mkdir")))
   (setq counsel-grep-base-command
         "rg -i -M 120 --no-heading --line-number --color never %s %s"
-        counsel-find-file-at-point t)
+        counsel-find-file-at-point t
+        ivy-format-function 'ivy-format-function-line)
   :custom
   (counsel-preselect-current-file t)
   (counsel-yank-pop-preselect-last t)
@@ -67,7 +68,44 @@
 
 (use-package ivy-rich
   :ensure t
+  :after counsel
   :config
+  (setq ivy-rich-path-style 'abbrev
+        ivy-rich-project-root-cache-mode t
+        ivy-rich-display-transformers-list
+        '(ivy-switch-buffer
+          (:columns
+           ((ivy-rich-candidate (:width 20))
+            (ivy-rich-switch-buffer-size (:width 7 :align right))
+            (ivy-rich-switch-buffer-indicators
+             (:width 2 :face error :align right))
+            (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+            (ivy-rich-switch-buffer-project (:width 8 :face success))
+            (ivy-rich-switch-buffer-path
+             (:width (lambda (x)
+                       (ivy-rich-switch-buffer-shorten-path
+                        x (ivy-rich-minibuffer-width 0.3))))))
+           :predicate (lambda (cand) (get-buffer cand)))
+          counsel-M-x
+          (:columns
+           ((counsel-M-x-transformer (:width 40))
+            (ivy-rich-counsel-function-docstring
+             (:face font-lock-doc-face))))
+          counsel-describe-function
+          (:columns
+           ((counsel-describe-function-transformer (:width 40))
+            (ivy-rich-counsel-function-docstring
+             (:face font-lock-doc-face))))
+          counsel-describe-variable
+          (:columns
+           ((counsel-describe-variable-transformer (:width 40))
+            (ivy-rich-counsel-variable-docstring
+             (:face font-lock-doc-face))))
+          counsel-recentf
+          (:columns
+           ((ivy-rich-candidate (:width 0.8))
+            (ivy-rich-file-last-modified-time
+             (:face font-lock-comment-face))))))
   (ivy-rich-mode t))
 
 (use-package ivy-xref
