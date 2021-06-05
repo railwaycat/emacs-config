866062b6cf220c2019fff56956676102a58e746b
Xin Xu <railwaycat@gmail.com>
2021-06-04 23:02:24 -0400

    not enable lsp for py by default

--- conf/lsp.el
+++ conf/lsp.el
@@ -5,9 +5,9 @@
 (use-package lsp-python-ms
   :ensure t
   :init (setq lsp-python-ms-auto-install-server t)
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-python-ms)
-                         (lsp-deferred)))
+  ;; :hook (python-mode . (lambda ()
+  ;;                        (require 'lsp-python-ms)
+  ;;                        (lsp-deferred)))
   :config
   (setq read-process-output-max (* 1024 1024)))
 
