21aa449bee6bf9b224116b4dd9455ae97bd10111
Xin Xu <railwaycat@gmail.com>
2021-10-01 17:01:22 -0400

    add wubi to pyim

--- conf/pyim.el
+++ conf/pyim.el
@@ -11,24 +11,31 @@
   (setq pyim-page-tooltip 'minibuffer)
   (setq pyim-page-length 9)
   ;; 探针设置
-  (setq-default pyim-english-input-switch-functions
-                '(pyim-probe-auto-english
-                  pyim-probe-program-mode
-                  pyim-probe-org-structure-template))
-  (setq-default pyim-punctuation-half-width-functions
-                '(pyim-probe-punctuation-line-beginning
-                  pyim-probe-punctuation-after-punctuation))
+  ;; (setq-default pyim-english-input-switch-functions
+  ;;               '(pyim-probe-auto-english
+  ;;                 pyim-probe-program-mode
+  ;;                 pyim-probe-org-structure-template))
+  ;; (setq-default pyim-punctuation-half-width-functions
+  ;;               '(pyim-probe-punctuation-line-beginning
+  ;;                 pyim-probe-punctuation-after-punctuation))
 
   ;; 仓颉设置
-  (use-package pyim-cangjiedict
+  ;; (use-package pyim-cangjiedict
+  ;;   :ensure t
+  ;;   :config
+  ;;   (pyim-cangjie5dict-enable))
+  ;; (setq pyim-default-scheme 'cangjie)
+
+  ;; 五笔设置
+  (use-package pyim-wbdict
     :ensure t
     :config
-    (pyim-cangjie5dict-enable))
-  ;; (setq pyim-default-scheme 'cangjie)
+    (pyim-wbdict-v98-morphe-enable))
+  (setq pyim-default-scheme 'wubi)
 
   ;; 拼音设置
   (use-package pyim-tsinghua-dict
     :config
     (pyim-tsinghua-dict-enable))
-  (setq pyim-default-scheme 'quanpin)
+  ;; (setq pyim-default-scheme 'quanpin)
   )
