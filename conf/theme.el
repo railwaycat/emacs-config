85618706533ecee27945a317da9b2538b5e6a467
Xin Xu <railwaycat@gmail.com>
2022-06-12 10:45:25 -0400

    unset bg color in terminal for modus operandi

--- conf/theme.el
+++ conf/theme.el
@@ -2,3 +2,9 @@
 ;; customized solarized
 (add-to-list 'load-path (concat user-emacs-directory "themes/solarized"))
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/solarized"))
+
+;; modus-operandi
+;; satisfy macOS terminal.app
+(if (not window-system)
+    (setq modus-themes-operandi-color-overrides
+      '((bg-main . "unspecified-bg"))))
