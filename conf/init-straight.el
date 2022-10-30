30fc02f07c20517865b6e4ed725b8ff183ecf0a7
Xin Xu <railwaycat@gmail.com>
2022-10-30 12:44:35 -0400

    now straight can be used as an alternative.

--- conf/init-straight.el
+++ conf/init-straight.el
@@ -7,6 +7,21 @@
 
 (setq package-enable-at-startup nil)
 
+
+(setq straight-build-dir (format "build-%s" emacs-version))
+
+;; Straight needs texinfo to build info files, however latest macOS no longer
+;; include texinfo. Texinfo installed by Homebrew is "keg-only" and will not in
+;; PATH. On macOS I will use this command to bootstrap and upgrade packages for
+;; Straight.
+
+;; PATH="$(brew --prefix)/opt/texinfo/bin:$PATH" emacs --batch -l ~/.emacs.d/init.el --eval '(straight-pull-all)'
+
+;; The following two lines add path for texinfo, I feel it may not that
+;; necessary.
+;; (add-to-list 'exec-path "/opt/homebrew/opt/texinfo/bin")
+;; (setenv "PATH" (concat "/opt/homebrew/opt/texinfo/bin:" (getenv "PATH")))
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
