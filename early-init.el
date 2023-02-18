9fecf8beb994176d79f309f01517401c1b8f2dbc
Xin Xu <railwaycat@gmail.com>
2023-02-18 12:20:16 -0500

    avoid warning

--- early-init.el
+++ early-init.el
@@ -4,6 +4,8 @@
 
 ;;; Code:
 
+;; avoid warning on macOS for emacs 28
+(when (eq system-type 'darwin) (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
 
 (provide 'early-init)
 ;;; early-init.el ends here
