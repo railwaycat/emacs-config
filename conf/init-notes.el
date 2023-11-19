63c1aaac9aaf9059f8ffb8822a6281453a30b7ef
Xin Xu <railwaycat@gmail.com>
2023-11-18 19:40:44 -0500

    notes: inbox file

--- conf/init-notes.el
+++ conf/init-notes.el
@@ -6,14 +6,27 @@
 
 
 ;; Location of notes
-(setq my/notes-directory (if user-with-dropbox
-                             "~/Dropbox/notes/"
-                           "~/notes/"))
+(setq-default my/notes-directory (if user-with-dropbox
+                                     "~/Dropbox/notes/"
+                                   "~/notes/"))
+
 
 ;; notes-grep and notes-find should already be defined by helm, ivy or
 ;; consult setup.
 (define-key global-map (kbd "C-c n g") #'notes-grep)
 (define-key global-map (kbd "C-c n f") #'notes-find)
+
+
+;; Open inbox file
+(defun notes-inbox ()
+  "Open the notes inbox file"
+  (interactive)
+  (let ((buffer (find-buffer-visiting (concat my/notes-directory "/inbox.md"))))
+    (if buffer
+        (switch-to-buffer buffer)
+      (find-file (concat my/notes-directory "/inbox.md"))
+      (goto-char (point-max)))))
+(define-key global-map (kbd "C-c n i") #'notes-inbox)
 
 
 (provide 'init-notes)
