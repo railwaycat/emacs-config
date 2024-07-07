14029d36453f483ef5468246fd3f075a556feb58
sinxccc <railwaycat@gmail.com>
2024-07-06 22:02:44 -0400

    increase line spacing for GUI
    The changes for override background of modus-operandi theme, I'm not
    sure if it works.

--- conf/init-theme.el
+++ conf/init-theme.el
@@ -12,11 +12,17 @@
 (add-to-list 'custom-theme-load-path
              (concat user-emacs-directory "themes/solarized"))
 
+
+(setq default-text-properties '(line-spacing 0.25 line-height 1.25))
+
 ;; modus-operandi
 ;; satisfy macOS terminal.app
-(if (not window-system)
-    (setq modus-themes-operandi-color-overrides
-      '((bg-main . "unspecified-bg"))))
+(when (not window-system)
+  (if (version< emacs-version "29.1")
+      (setq modus-themes-operandi-color-overrides
+            '((bg-main . "unspecified-bg")))
+    (setq modus-operandi-palette-overrides
+          '((bg-main "unspecified-bg")))))
 
 
 (provide 'init-theme)
