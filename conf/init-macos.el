a666178538ea89af676dfad0826f0052d93f97ea
sinxccc <railwaycat@gmail.com>
2024-12-10 17:21:15 -0500

    arrange keybinding for frames in macOS

--- conf/init-macos.el
+++ conf/init-macos.el
@@ -29,8 +29,6 @@
 (global-set-key [(hyper c)] 'kill-ring-save)
 (global-set-key [(hyper s)] 'save-buffer)
 (global-set-key [(hyper l)] 'goto-line)
-(global-set-key [(hyper w)] 'delete-frame)
-(global-set-key [(hyper n)] 'make-frame-command)
 (global-set-key [(hyper z)] 'undo)
 ;; (global-set-key [(hyper x)] 'kill-region)
 (global-set-key [(hyper o)] 'find-file)
@@ -38,11 +36,14 @@
 ;; (global-set-key [(hyper g)] 'isearch-repeat-forward)
 ;; (global-set-key [(hyper .)] 'keyboard-quit)
 ;; (global-set-key [(hyper q)] 'save-buffers-kill-emacs)
-(global-set-key [(hyper m)] 'suspend-frame)
-;; (global-set-key [(hyber n)] 'make-frame-command)
 (global-set-key [(hyper {)] 'previous-buffer)
 (global-set-key [(hyper })] 'next-buffer)
 (global-set-key [(hyper r)] 'revert-buffer)
+;; frames
+(global-set-key (kbd "H-`") 'other-frame)
+(global-set-key [(hyper n)] 'make-frame-command)
+(global-set-key [(hyper w)] 'delete-frame)
+(global-set-key [(hyper m)] 'suspend-frame)
 
 
 (setq mac-option-modifier 'meta)
