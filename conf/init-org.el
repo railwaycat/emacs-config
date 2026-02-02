7ce4ffa89a921ec6dacd29d79c05c05cb03e5cba
sinxccc <railwaycat@gmail.com>
2026-02-02 09:39:56 -0500

    show all stars in org indent mode

--- conf/init-org.el
+++ conf/init-org.el
@@ -22,7 +22,8 @@
 ;; styles
 ;; content indent in headline, but only 1 space.
 (setq org-startup-indented t
-      org-indent-indentation-per-level 1)
+      org-indent-indentation-per-level 1
+      org-indent-mode-turns-on-hiding-stars nil)
 ;; no new line between entries (heading/list)
 (setq org-blank-before-new-entry '((heading . nil)
                                    (plain-list-item . nil)))
