93d493fb30ca0df52d024d87933de80a034a939c
sinxccc <railwaycat@gmail.com>
2026-03-10 10:32:02 -0400

    cleanup init-utils

--- conf/init-utils.el
+++ conf/init-utils.el
@@ -19,13 +19,6 @@
     (diminish 'projectile-mode
               '(:eval (concat " [" (projectile-project-name) "]")))))
 
-
-;; session
-;; (use-package session
-;;   :hook
-;;   (after-init . session-initialize))
-
-
 ;; bm
 (ensure-package 'bm)
 (use-package bm
@@ -43,24 +36,21 @@
   ("C-c R" . rg))
 
 ;; Dashboard
+;; (ensure-package 'dashboard)
 ;; (use-package dashboard
 ;;   :init
-;;   (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*"))
-;;         dashboard-items '((recents . 5)
+;;   (setq dashboard-items '((recents . 5)
 ;;                           (bookmarks . 7)
 ;;                           (projects . 5))
+;;         dashboard-projects-backend 'projectile
 ;;         dashboard-banner-logo-title "C-x C-c to exit Emacs"
-;;         dashboard-set-footer nil)
-;;   (if (window-system)
-;;       (setq dashboard-startup-banner (concat user-emacs-directory "logo1.png"))
-;;     (setq dashboard-startup-banner (concat user-emacs-directory "logo3.txt")))
+;;         dashboard-set-footer nil
+;;         dashboard-startup-banner
+;;         (if window-system
+;;             (concat user-emacs-directory "logo1.png")
+;;           (concat user-emacs-directory "logo3.txt")))
 ;;   :bind
-;;   ("C-c C-d" . (lambda ()
-;;                  (interactive)
-;;                  (switch-to-buffer dashboard-buffer-name)
-;;                  (dashboard-insert-startupify-lists)
-;;                  ;; (dashboard-refresh-buffer)
-;;                  (delete-other-windows)))
+;;   ("C-c C-d" . dashboard-open)
 ;;   :config
 ;;   (dashboard-setup-startup-hook))
 
@@ -83,98 +73,6 @@
 (use-package vundo
   :bind
   ("C-x u" . vundo))
-
-
-;; Popper
-;; Setup from roife@github
-;; (ensure-package 'popper)
-;; (use-package popper
-;;   :bind (:map popper-mode-map
-;;               ("M-<tab>" . popper-cycle)
-;;               ("C-M-i" . popper-cycle)
-;;               ("M-`" . popper-toggle-type))
-;;   :hook (emacs-startup . popper-mode)
-;;   :init
-;;   (setq popper-reference-buffers
-;;         '("\\*Messages\\*"
-;;           "Output\\*$" "\\*Pp Eval Output\\*$"
-;;           "\\*Compile-Log\\*"
-;;           "\\*Completions\\*"
-;;           "\\*Warnings\\*"
-;;           "\\*Async Shell Command\\*"
-;;           "\\*Apropos\\*"
-;;           "\\*Backtrace\\*"
-;;           "\\*Calendar\\*"
-;;           "\\*Embark Actions\\*"
-;;           "\\*Finder\\*"
-;;           "\\*Kill Ring\\*"
-;;           "\\*Go-Translate\\*"
-
-;;           bookmark-bmenu-mode
-;;           comint-mode
-;;           compilation-mode
-;;           help-mode helpful-mode
-;;           tabulated-list-mode
-;;           Buffer-menu-mode
-
-;;           gnus-article-mode devdocs-mode
-;;           grep-mode occur-mode rg-mode ag-mode pt-mode
-
-;;           "^\\*Process List\\*" process-menu-mode
-;;           list-environment-mode cargo-process-mode
-
-;;           "^\\*eshell.*\\*.*$" eshell-mode
-;;           "^\\*shell.*\\*.*$"  shell-mode
-;;           "^\\*terminal.*\\*.*$" term-mode
-;;           "^\\*vterm.*\\*.*$"  vterm-mode
-;;           "^\\*eldoc.*\\*.*$" eldoc-mode
-
-;;           "\\*Flycheck errors\\*$" " \\*Flycheck checker\\*$"
-;;           "\\*Paradox Report\\*$" "\\*package update results\\*$" "\\*Package-Lint\\*$"
-;;           "\\*[Wo]*Man.*\\*$"
-;;           "\\*ert\\*$" overseer-buffer-mode
-;;           "\\*gud-debug\\*$"
-;;           "\\*lsp-help\\*$" "\\*lsp session\\*$"
-;;           "\\*quickrun\\*$"
-;;           "\\*tldr\\*$"
-;;           "\\*vc-.*\\*$"
-;;           "^\\*elfeed-entry\\*$"
-;;           "^\\*macro expansion\\**"
-
-;;           "\\*Gofmt Errors\\*$" "\\*Go Test\\*$" godoc-mode
-;;           "\\*docker-.+\\*"
-;;           "\\*prolog\\*" inferior-python-mode inf-ruby-mode swift-repl-mode
-;;           "\\*rustfmt\\*$" rustic-compilation-mode rustic-cargo-clippy-mode
-;;           rustic-cargo-outdated-mode rustic-cargo-test-mode
-;; 	  ))
-;;   :custom
-;;   (popper-window-height (lambda (win)
-;;                           (fit-window-to-buffer
-;;                            win
-;;                            (floor (frame-height) 2)
-;;                            (floor (frame-height) 2))))
-;;   :config
-;;   ;; mode-line indicator
-;;   (with-eval-after-load 'popper
-;;     (setq popper-mode-line
-;;           '(:propertize " POP |"
-;;                         face +mode-line-meta-active-face)))
-
-;;   ;; Enable indicator in minibuffer
-;;   (popper-echo-mode t)
-
-;;   ;; HACK: close popper with `C-g'
-;;   (defun +popper-close-window-hack (&rest _)
-;;     "Close popper window via `C-g'."
-;;     (when (and (called-interactively-p 'interactive)
-;;                (not (region-active-p))
-;;                popper-open-popup-alist)
-;;       (let ((window (caar popper-open-popup-alist)))
-;;         (when (window-live-p window)
-;;           (delete-window window)))))
-;;   (advice-add #'keyboard-quit :before #'+popper-close-window-hack)
-;;   )
-
 
 ;; ox-hugo
 (ensure-package 'ox-hugo)
