92842d52c1d3eb9dcdb9ee44c0058b95ea526f16
sinxccc <railwaycat@gmail.com>
2026-03-10 10:17:19 -0400

    cleanup init-terminal

--- conf/init-terminal.el
+++ conf/init-terminal.el
@@ -11,12 +11,20 @@
 
 ;; xterm mouse
 (xterm-mouse-mode t)
-(global-set-key [mouse-4] (lambda ()
-                            (interactive)
-                            (scroll-down 1)))
-(global-set-key [mouse-5] (lambda ()
-                            (interactive)
-                            (scroll-up 1)))
+
+;; mouse wheel
+(setq mouse-wheel-scroll-amount '(2 ((shift) . 1)) ;; one line at a time
+      mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
+      mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+
+;; For older Emacs versions in terminal, mouse wheel may still need
+;; explicit mouse-4/mouse-5 bindings.
+;; (global-set-key [mouse-4] (lambda ()
+;;                             (interactive)
+;;                             (scroll-down 1)))
+;; (global-set-key [mouse-5] (lambda ()
+;;                             (interactive)
+;;                             (scroll-up 1)))
 
 
 (provide 'init-terminal)
