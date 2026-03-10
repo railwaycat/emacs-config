1119544e5a8c697a1d95ab17e4bc4706991ae9ff
sinxccc <railwaycat@gmail.com>
2026-03-09 22:44:42 -0400

    removed flycheck config
    not be used for a while

--- conf/flycheck.el
+++
@@ -1,8 +0,0 @@
-(use-package flycheck
-  :ensure t
-  :hook (prog-mode . flycheck-mode)
-  :custom
-  (flycheck-disabled-checkers '(emacs-lisp-checkdoc
-                                c/c++-clang
-                                c/c++-gcc
-                                c/c++-cppcheck)))
