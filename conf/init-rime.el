4968fe057fa48fe1ad096ff1685f81e72512f773
sinxccc <railwaycat@gmail.com>
2026-06-21 23:36:37 -0400

    Fix rime-menu in TUI; add a force enable keybinding

--- conf/init-rime.el
+++ conf/init-rime.el
@@ -6,11 +6,21 @@
 
 
 (ensure-package 'rime)
+
+;; A wrapper function to satisfy TUI
+(defun rime-send-menu-keybinding ()
+  "Send `C-`' to librime to open the Rime schema/options menu."
+  (interactive)
+  (rime-lib-process-key ?\` 4)          ; ?\` = grave, 4 = control mask (1<<2)
+  (rime--redisplay)
+  (rime--refresh-mode-state))
+
 (use-package rime
   :defer t
   :bind
   (:map rime-mode-map
-        ("C-c `" . 'rime-send-keybinding))
+        ("C-c `" . 'rime-send-menu-keybinding)
+        ("M-j" . 'rime-force-enable))
   :custom
   (rime-librime-root (concat user-emacs-directory "librime/dist"))
   (rime-title "[R]")
