d69902ff74d6893075f20ac1c997e328c24670e4
sinxccc <railwaycat@gmail.com>
2026-09-01 22:50:18 -0400

    adjust for themes in TUI

--- conf/init-theme.el
+++ conf/init-theme.el
@@ -13,13 +13,22 @@
              (concat user-emacs-directory "themes/solarized"))
 
 
-;; modus-operandi
-;; fallback config for no-truecolor terminals
+;; Do not use an underline as a mode-line border on text terminals.
+(custom-set-faces
+ '(mode-line ((((type tty)) :underline nil)))
+ '(mode-line-active ((((type tty)) :underline nil)))
+ '(mode-line-inactive ((((type tty)) :underline nil))))
+
+
+;; modus theme
+;; Use the terminal's default background color
 ;; (when (not window-system)
-;;   (if (version< emacs-version "29.1")
+;;   (if (< emacs-major-version 30)
 ;;       (setq modus-themes-operandi-color-overrides
+;;             '((bg-main . "unspecified-bg"))
+;;             modus-themes-vivendi-color-overrides
 ;;             '((bg-main . "unspecified-bg")))
-;;     (setq modus-operandi-palette-overrides
+;;     (setq modus-themes-common-palette-overrides
 ;;           '((bg-main "unspecified-bg")))))
 
 
